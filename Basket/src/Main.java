a98940340a8dd5051be8e26391ac71ea67f6031a
safaa <s.hriez@htu.edu.jo>
2024-12-18 12:18:01 +0300

    safaa edits

--- Basket/src/Main.java
+++ Basket/src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
     Basket<Integer, Double> b = new Basket<>();
-    
+    b.add(2.0);
+    b.add(3.5);
+
+        System.out.println(b);
 
     }
 }
